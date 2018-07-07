2be4976d175c6c74573f8ab503c2a25c3b341612
Mischa Radenovic <miroslav.radenovic@gmail.com>
2018-07-07 17:18:33 -0400

    refactor: Fetch restaurants from server

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -16,19 +16,14 @@ class DBHelper {
    * Fetch all restaurants.
    */
   static fetchRestaurants(callback) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', DBHelper.DATABASE_URL);
-    xhr.onload = () => {
-      if (xhr.status === 200) { // Got a success response from server!
-        const json = JSON.parse(xhr.responseText);
-        const restaurants = json.restaurants;
-        callback(null, restaurants);
-      } else { // Oops!. Got an error from server.
-        const error = (`Request failed. Returned status of ${xhr.status}`);
-        callback(error, null);
-      }
-    };
-    xhr.send();
+    // fetch data
+    fetch('http://localhost:1337/restaurants')
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        callback(null, data);
+      });
   }
 
   /**
@@ -150,7 +145,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    return (`/img/${restaurant.photograph}.jpg`);
   }
 
   /**
