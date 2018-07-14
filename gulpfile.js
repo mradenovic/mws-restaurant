ee8db9d5d6835bb4a3c50e5ad66f4b43cd764308
Mischa Radenovic <miroslav.radenovic@gmail.com>
2018-07-14 15:14:08 -0400

    chore: Add build process

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+
+gulp.task('default', ['styles'], function() {
+	gulp.watch('sass/**/*.scss', ['styles']);
+
+	browserSync.init({
+		browser: ["google-chrome"],
+		server: './',
+		port: '8000'
+	});
+});
+
+gulp.task('styles', function() {
+	gulp.src('sass/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions']
+		}))
+		.pipe(gulp.dest('./css'))
+		.pipe(browserSync.stream());
+});
