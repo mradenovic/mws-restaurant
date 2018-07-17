5d41af0e0133eeb8e5c8e660aeedea5215e19c10
Mischa Radenovic <miroslav.radenovic@gmail.com>
2018-07-17 09:39:53 -0400

    refactor: Redesign build process

--- gulpfile.js
+++ gulpfile.js
@@ -5,30 +5,27 @@ var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
 var eslint = require('gulp-eslint');
+// var babel = require('gulp-babel');
+// var babelify = require('babelify');
+// var browserify = require("browserify");
+// var connect = require("gulp-connect");
+// var source = require("vinyl-source-stream");
+// var buffer = require("vinyl-buffer");
+// var uglify = require("gulp-uglify");
+// var sourcemaps = require("gulp-sourcemaps");
 
-gulp.task('default', ['styles', 'lint'], function () {
-  gulp.watch('sass/**/*.scss', ['styles']);
-  gulp.watch('js/**/*.js', ['lint']);
+gulp.task('default', ['copy'], function () {
+  gulp.watch('src/**/*.*', ['reload']);
 
   browserSync.init({
     browser: ['google-chrome'],
-    server: './',
+    server: 'dist',
     port: '8000'
   });
 });
 
-gulp.task('styles', function () {
-  gulp.src('sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions']
-    }))
-    .pipe(gulp.dest('./css'))
-    .pipe(browserSync.stream());
-});
-
 gulp.task('lint', function () {
-  return gulp.src(['js/**/*.js'])
+  return gulp.src(['src/js/**/*.js'])
   // eslint() attaches the lint output to the eslint property
   // of the file object so it can be used by other modules.
     .pipe(eslint())
@@ -38,4 +35,35 @@ gulp.task('lint', function () {
   // To have the process exit with an error code (1) on
   // lint error, return the stream and pipe to failOnError last.
   // .pipe(eslint.failOnError());
+});
+
+gulp.task('img', () => {
+  gulp.src('src/img/**/*.jpg')
+    .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('html', () => {
+  gulp.src('src/*.html')
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('sass', () => {
+  gulp.src('src/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions']
+    }))
+    .pipe(gulp.dest('dist/css'))
+});
+
+gulp.task('js', ['lint'], () => {
+  gulp.src('src/**/*.js')
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('copy', ['img', 'html', 'sass', 'js']);
+
+gulp.task('reload', ['copy'], function (done) {
+  browserSync.reload();
+  done();
 });
