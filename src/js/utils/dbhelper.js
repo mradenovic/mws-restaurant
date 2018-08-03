fb807fb0ab9f925164405030023bef2be9dbd79d
Mischa Radenovic <miroslav.radenovic@gmail.com>
2018-08-03 16:41:52 -0400

    refactor: Remove redundant code

--- src/js/utils/dbhelper.js
+++ src/js/utils/dbhelper.js
@@ -19,21 +19,6 @@ export default class DBHelper {
     let img = restaurant.photograph || restaurant.id;
     return (`/img/${img}.webp`);
   }
-
-  /**
-   * Map marker for a restaurant.
-   */
-  static mapMarkerForRestaurant(google, restaurant, map) {
-    const marker = new google.maps.Marker({
-      position: restaurant.latlng,
-      title: restaurant.name,
-      url: DBHelper.urlForRestaurant(restaurant),
-      map: map,
-      animation: google.maps.Animation.DROP}
-    );
-    return marker;
-  }
-
 }
 
 /** Databse Service
